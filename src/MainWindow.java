dd89286966181f25813b306d347fcc138421b434
Tripdesk <kontakt@tripdesk.pl>
2019-05-19 13:13:38 +0200

    KURWA DZIALAJ

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -4,6 +4,7 @@ import net.miginfocom.swing.*;
  * Created by JFormDesigndsaf;']
  * .DSfg ];[\L.'WRAT EGer on Sun May 19 12:56:17 CEST 2019
  * KURWAfasdfasdfasdf asd
+ * asdkj;asd;lkasnd;laksnda';slkdas'lkdnas'ldknas'ldkasd
  */
 
 
